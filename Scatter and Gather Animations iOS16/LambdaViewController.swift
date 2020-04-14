f14ab061fa605eb186f9c1ac276419cc0860d3a6
Stephanie Ballard <user.name>
2020-04-14 16:14:48 -0700

    lambda logo fading

---
+++ Scatter and Gather Animations iOS16/LambdaViewController.swift
@@ -0,0 +1,53 @@
+//
+//  LambdaViewController.swift
+//  Scatter and Gather Animations iOS16
+//
+//  Created by Stephanie Ballard on 4/14/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+import UIKit
+
+class LambdaViewController: UIViewController {
+
+    var isScattered: Bool = false
+    
+    @IBOutlet weak var letterLTextLabel: UILabel!
+    @IBOutlet weak var letterATextLabel: UILabel!
+    @IBOutlet weak var letterMTextLabel: UILabel!
+    @IBOutlet weak var letterBTextLabel: UILabel!
+    @IBOutlet weak var letterDTextLabel: UILabel!
+    @IBOutlet weak var secondLetterATextLabel: UILabel!
+    @IBOutlet weak var lambdaImage: UIImageView!
+    
+    @IBOutlet var lambdaLetters: [UILabel]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func startAnimations() {
+        lambdaImage.alpha = 0
+        UIView.animate(withDuration: 3) {
+            self.lambdaImage.alpha = 1
+        }
+    }
+    
+    @IBAction func toggleButtonPressed(_ sender: UIBarButtonItem) {
+        startAnimations()
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
